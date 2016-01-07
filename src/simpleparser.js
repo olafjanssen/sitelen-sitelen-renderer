0b917890fbdb597f6a21524061c22dcacfed0a31
Olaf Janssen <o.t.a.janssen@gmail.com>
2016-01-07 17:06:07 +0100

    Starting to refactor the Simple Parser to accommodate all possible rendering orders.

--- src/simpleparser.js
+++ src/simpleparser.js
@@ -1,7 +1,23 @@
 'use strict';
 
+var smallModifiers = ['kon', 'lili', 'mute', 'sin']
+
+function convertNounPhrase(part) {
+    var instructions = [];
+    instructions.push({rule: 'openContainer', glyph: part.sep});
+    part.tokens.forEach(function (token) {
+        if (smallModifiers.indexOf(token) >= 0) {
+            instructions[instructions.length - 1].modifier = token;
+        } else {
+            instructions.push({rule: 'addGlyph', glyph: token});
+        }
+    });
+    instructions.push({rule: 'closeContainer'});
+
+    return instructions;
+}
+
 function convertToInstructions(structuredSentence) {
-    var smallModifiers = ['kon', 'lili', 'mute', 'sin']
 
     var instructions = [],
         size;
@@ -10,32 +26,9 @@ function convertToInstructions(structuredSentence) {
 
         switch (part.part) {
             case 'subject':
-                size = part.tokens.length == 1 ? 'regular' : part.tokens.length < 4 ? 'wide' : 'double';
-                instructions.push({rule: 'openContainer', glyph: undefined, size: size, children: part.tokens.length});
-                part.tokens.forEach(function (token) {
-                    if (smallModifiers.indexOf(token) >= 0) {
-                        instructions[instructions.length - 1].modifier = token;
-                    } else {
-                        instructions.push({rule: 'addGlyph', glyph: token});
-                    }
-                });
-                instructions.push({rule: 'closeContainer'});
-                break;
             case 'verbPhrase':
-                size = part.tokens.length == 1 ? 'regular' : part.tokens.length < 4 ? 'wide' : 'double';
-                instructions.push({rule: 'openContainer', glyph: part.sep, size: size, children: part.tokens.length});
-                part.tokens.forEach(function (token) {
-                    instructions.push({rule: 'addGlyph', glyph: token});
-                });
-                instructions.push({rule: 'closeContainer'});
-                break;
             case 'directObject':
-                size = part.tokens.length < 4 ? 'wide' : 'double';
-                instructions.push({rule: 'openContainer', glyph: part.sep, size: size, children: part.tokens.length});
-                part.tokens.forEach(function (token) {
-                    instructions.push({rule: 'addGlyph', glyph: token});
-                });
-                instructions.push({rule: 'closeContainer'});
+                instructions.push.apply(instructions, convertNounPhrase(part));
                 break;
             case 'punctuation':
                 instructions.push({rule: 'addPunctuation', glyph: part.token === '.' ? 'period' : '', size: 'wide'});
@@ -46,6 +39,34 @@ function convertToInstructions(structuredSentence) {
         }
     });
 
+    console.log(instructions);
+    // determine container sizes
+    for (var i = 0; i < instructions.length; i++) {
+        if (instructions[i].rule === 'openContainer') {
+            var closure = 0, glyphs = 0;
+            for (var j = i; j < instructions.length; j++) {
+                switch (instructions[j].rule) {
+                    case 'openContainer':
+                        closure++;
+                        break;
+                    case 'closeContainer':
+                        closure--;
+                        if (closure === 0) {
+                            instructions[i].size = glyphs === 1 ? 'regular' : glyphs > 3 ? 'double' : 'wide';
+                            instructions[i].children = glyphs;
+                            closure = -1;
+                        }
+                        break;
+                    case 'addGlyph':
+                        glyphs++;
+                        break;
+                }
+                if (closure === -1) {
+                    break;
+                }
+            }
+        }
+    }
     return instructions;
 }
 
