d49a11eaaa7c47251bd1867e2052e3c800b90c9c
Olaf Janssen <o.t.a.janssen@gmail.com>
2016-01-09 00:24:33 +0100

    Creating scaling independent SVGs, by converting all nonuniform brushes to uniform ones and adding parameters to the SVG files manually (well using a node js script).

---
+++ resources/derivative works/updatesvg.js
@@ -0,0 +1,52 @@
+/**
+ * Created by olafjanssen on 08/01/16.
+ */
+var fs = require("fs");
+var glob = require('glob');
+
+// renaming wronly named files
+//var files = glob.sync('../../images/glyphs/*tp-wg-pre-.svg');
+//files.forEach(function (file) {
+//    var token = file.substring(20, file.length - 14);
+//    fs.renameSync(file, '../../images/glyphs/tp-wg-pre-' + token + '.svg');
+//    console.log(token);
+//});
+
+var files = glob.sync('../../images/glyphs/tp-wg-pre-*.svg');
+files.forEach(function (filename) {
+    var token = filename.substring(30);
+    console.log(token);
+
+    var buf = fs.readFileSync(filename, "utf8");
+
+    var DOMParser = require('xmldom').DOMParser;
+    var doc = new DOMParser().parseFromString(buf, 'image/svg+xml');
+
+    var svg = doc.documentElement;
+
+    var viewBoxValues = svg.attributes[5].nodeValue.split(' ');
+
+    var strokeWidth = 2;
+    var newViewBoxValues = [parseFloat(viewBoxValues[0]) - strokeWidth, parseFloat(viewBoxValues[1]) - strokeWidth,
+        parseFloat(viewBoxValues[2]) + strokeWidth * 2, parseFloat(viewBoxValues[3]) + strokeWidth * 2].join(' ');
+
+    svg.attributes[5].nodeValue = newViewBoxValues;
+    svg.attributes[5].value = newViewBoxValues;
+
+    var style = doc.createElement('style');
+    style.textContent = 'path { stroke-width: 2; vector-effect: non-scaling-stroke;}';
+
+    svg.appendChild(style);
+
+
+    var svgolib = require('svgo'),
+        svgo = new svgolib();
+
+    svgo.optimize(doc.toString(), function (result) {
+        "use strict";
+        fs.writeFileSync('../../images/glyphs/tp-wg-' + token, result.data);
+    });
+
+});
+
+
