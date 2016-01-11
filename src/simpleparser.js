457000c108623e69f49cd19cc662240cbd26d0c9
Olaf Janssen <o.t.a.janssen@gmail.com>
2016-01-11 10:55:04 +0100

    Abstracting algorithm so that it does not require to layout tokens but general units with a size.

--- src/simpleparser.js
+++ src/simpleparser.js
@@ -1,123 +1,161 @@
 "use strict";
 
-var smallModifiers = ['kon', 'lili', 'mute', 'sin'],
-    narrowModifiers = ['wan', 'tu', 'anu', 'en', 'kin'];
 
-function getSizeOf(token) {
-    if (smallModifiers.indexOf(token) > -1) {
-        return [1, 0.5];
-    } else if (narrowModifiers.indexOf(token) > -1) {
-        return [0.5, 1];
-    } else {
-        return [1, 1];
+function layoutContainer(units) {
+
+    var options = [], hash = {}, minSurface = 1e6;
+
+    function normalizeOption(option) {
+        option = JSON.parse(option);
+
+        var minSize = 1;
+        option.state.units.forEach(function (glyph) {
+            if (glyph.size[0] < minSize) {
+                minSize = glyph.size[0];
+            }
+            if (glyph.size[1] < minSize) {
+                minSize = glyph.size[1];
+            }
+        });
+
+        option.size = [option.size[0] / minSize, option.size[1] / minSize];
+        option.surface /= minSize * minSize;
+        option.state.units.forEach(function (glyph) {
+            glyph.size[0] /= minSize;
+            glyph.size[1] /= minSize;
+            glyph.position[0] /= minSize;
+            glyph.position[1] /= minSize;
+        });
+
+        return option;
     }
-}
 
-var options = [], hash = {}, minSurface = 1e6;
+    function go(units, state, iterator) {
+        var newSize, newState, newForbidden, i, j,
+            goesDown, index, length,
+            prevSize, unitPosition;
 
-function go(units, state, iterator) {
-    console.log(state, iterator);
-    var newSize, newState, newForbidden, i, j;
+        // place the first unit directly
+        if (!state) {
+            var singleSize = units[0].size;
+            newState = {
+                units: [{unit: units[0], size: singleSize, position: [0, 0]}],
+                size: singleSize,
+                forbidden: [JSON.stringify(singleSize)]
+            };
 
-    if (!state) {
-        var singleSize = getSizeOf(units[0]);
-        newState = {
-            units: [{token: units[0], size: singleSize, position: [0, 0]}],
-            size: singleSize,
-            forbidden: [JSON.stringify(singleSize)]
-        };
-
-        for (j = 1; j < units.length; j++) {
-            go(units, newState, {goesDown: false, index: 1, length: j});
-            go(units, newState, {goesDown: true, index: 1, length: j});
+            for (j = 1; j < units.length; j++) {
+                go(units, newState, {goesDown: false, index: 1, length: j});
+                go(units, newState, {goesDown: true, index: 1, length: j});
+            }
+            return;
         }
-        return;
-    }
 
-    var goesDown = iterator.goesDown,
-        index = iterator.index,
+        // set up aliases, start parameters and clone the state
+        goesDown = iterator.goesDown;
+        index = iterator.index;
         length = iterator.length;
+        newState = JSON.parse(JSON.stringify(state));
+        newSize = newState.size;
+        newForbidden = newState.forbidden;
+        prevSize = units[index].size;
+        unitPosition = [!goesDown ? state.size[0] : 0, goesDown ? state.size[1] : 0];
 
-    newState = JSON.parse(JSON.stringify(state));
-    newSize = newState.size;
-    newForbidden = newState.forbidden;
+        // determine size sum and check if all following glyphs are compatible
+        var sizeSum = [0, 0], addSize;
+        for (i = index; i < index + length; i++) {
+            addSize = units[i].size;
+            if ((goesDown && addSize[1] !== prevSize[1]) ||
+                (!goesDown && addSize[0] !== prevSize[0])) {
+                return;
+            }
+            sizeSum = [sizeSum[0] + addSize[0], sizeSum[1] + addSize[1]];
+        }
 
-    var prevSize = getSizeOf(units[index]),
-        glyphPosition = [!goesDown ? state.size[0] : 0, goesDown ? state.size[1] : 0];
+        // now add the units one by one
+        for (i = index; i < index + length; i++) {
+            var glyphSize = [], add = 0,
+                addAxis = goesDown ? 1 : 0,
+                fixedAxis = goesDown ? 0 : 1;
 
-    var sizeSum = [0, 0], addSize;
-    // determine size sum
-    for (i = index; i < index + length; i++) {
-        addSize = getSizeOf(units[i]);
-        if ((goesDown && addSize[1] !== prevSize[1]) ||
-            (!goesDown && addSize[0] !== prevSize[0])) {
+            addSize = units[i].size;
+
+            add = addSize[addAxis] * state.size[fixedAxis] / sizeSum[fixedAxis];
+            glyphSize[addAxis] = add;
+            glyphSize[fixedAxis] = addSize[fixedAxis] * add / addSize[addAxis];
+
+            if (i === index) {
+                newSize[addAxis] += add;
+            }
+            if (goesDown && newForbidden.indexOf(JSON.stringify(unitPosition)) > -1) {
+                return;
+            }
+
+            prevSize = addSize;
+            newState.units.push({unit: units[i], size: glyphSize, position: unitPosition});
+
+            // update glyph position to slot for next glyph
+            unitPosition = [unitPosition[0] + (goesDown ? glyphSize[0] : 0),
+                unitPosition[1] + (!goesDown ? glyphSize[1] : 0)];
+
+            // forbid next glyphs to start at the lower-right corner of a previous glyph (this breaks the reading flow rules
+            newForbidden.push(JSON.stringify([unitPosition[0] + (!goesDown ? glyphSize[0] : 0),
+                unitPosition[1] + (goesDown ? glyphSize[1] : 0)]));
+        }
+
+        // if all units are used up we can stop and add the final result
+        if (index + length === units.length) {
+            var newOption = {
+                state: newState,
+                size: newSize,
+                ratio: newSize[0] / newSize[1],
+                normedRatio: newSize[0] / newSize[1] < 1 ? newSize[0] / newSize[1] : newSize[1] / newSize[0],
+                surface: newSize[0] * newSize[1]
+            };
+
+            newOption = normalizeOption(JSON.stringify(newOption));
+
+            minSurface = Math.min(minSurface, newOption.surface);
+
+            // surface can't be 3 times larger than the optimum
+            if (!hash[JSON.stringify(newOption)] && newOption.surface / minSurface < 3) {
+                options.push(newOption);
+                hash[JSON.stringify(newOption)] = newOption;
+            }
             return;
         }
-        sizeSum = [sizeSum[0] + addSize[0], sizeSum[1] + addSize[1]];
-    }
 
-    var actualAdded = 0;
-    // now add the glyphs one by one
-    for (i = index; i < index + length; i++) {
-        var glyphSize = [], add = 0,
-            addAxis = goesDown ? 1 : 0,
-            fixedAxis = goesDown ? 0 : 1;
-
-        addSize = getSizeOf(units[i]);
-
-        add = addSize[addAxis] * state.size[fixedAxis] / sizeSum[fixedAxis];
-        glyphSize[addAxis] = add;
-        glyphSize[fixedAxis] = addSize[fixedAxis] * add / addSize[addAxis];
-
-        if (i === index) {
-            newSize[addAxis] += add;
+        for (j = 1; j < units.length - index; j++) {
+            go(units, newState, {goesDown: false, index: index + length, length: j});
+            go(units, newState, {goesDown: true, index: index + length, length: j});
         }
-        if (goesDown && newForbidden.indexOf(JSON.stringify(glyphPosition)) > -1) {
-            return;
-        }
-
-        prevSize = addSize;
-        newState.units.push({token: units[i], size: glyphSize, position: glyphPosition});
-        actualAdded++;
-
-        // update glyph position
-        glyphPosition = [glyphPosition[0] + (goesDown ? glyphSize[0] : 0),
-            glyphPosition[1] + (!goesDown ? glyphSize[1] : 0)];
-
-        newForbidden.push(JSON.stringify([glyphPosition[0] + (!goesDown ? glyphSize[0] : 0), glyphPosition[1] + (goesDown ? glyphSize[1] : 0)]));
     }
 
-    if (index + actualAdded === units.length) {
-        var newOption = {
-            state: newState,
-            size: newSize,
-            ratio: newSize[0] / newSize[1],
-            normedRatio: newSize[0] / newSize[1] < 1 ? newSize[0] / newSize[1] : newSize[1] / newSize[0],
-            surface: newSize[0] * newSize[1]
-        };
-        console.log('ADD OPTION ', newOption);
-
-        newOption = normalizeOption(JSON.stringify(newOption));
-
-        minSurface = Math.min(minSurface, newOption.surface);
-
-        // surface can't be 3 times larger than the optimum
-        if (!hash[JSON.stringify(newOption)] && newOption.surface / minSurface < 3) {
-            options.push(newOption);
-            hash[JSON.stringify(newOption)] = newOption;
-        }
-        return;
-    }
-
-    for (j = 1; j < units.length - index; j++) {
-        go(units, newState, {goesDown: false, index: index + actualAdded, length: j});
-        go(units, newState, {goesDown: true, index: index + actualAdded, length: j});
-    }
+    go(units);
+    return options;
 }
-
 
 function convertNounPhrase(tokens) {
-    go(tokens);
+    var smallModifiers = ['kon', 'lili', 'mute', 'sin'],
+        narrowModifiers = ['wan', 'tu', 'anu', 'en', 'kin'],
+        options;
+
+    function getSizeOf(token) {
+        if (smallModifiers.indexOf(token) > -1) {
+            return [1, 0.5];
+        } else if (narrowModifiers.indexOf(token) > -1) {
+            return [0.5, 1];
+        } else {
+            return [1, 1];
+        }
+    }
+
+    var units = [];
+    tokens.forEach(function (token) {
+        units.push({type: 'word-glyph', token: token, size: getSizeOf(token)});
+    });
+
+    options = layoutContainer(units);
 
     options.sort(function (a, b) {
         return a.surface - b.surface;
@@ -140,37 +178,13 @@ function renderOption(option) {
         element.style.height = glyph.size[1] + 'em';
         element.style.left = glyph.position[0] + 'em';
         element.style.top = glyph.position[1] + 'em';
-        element.setAttribute('data-toki-word', glyph.token);
+        element.setAttribute('data-toki-word', glyph.unit.token);
 
         container.appendChild(element);
     });
     document.getElementById('sitelen').appendChild(container);
 }
 
-function normalizeOption(option) {
-    option = JSON.parse(option);
-
-    var minSize = 1;
-    option.state.units.forEach(function (glyph) {
-        if (glyph.size[0] < minSize) {
-            minSize = glyph.size[0];
-        }
-        if (glyph.size[1] < minSize) {
-            minSize = glyph.size[1];
-        }
-    });
-
-    option.size = [option.size[0] / minSize, option.size[1] / minSize];
-    option.surface /= minSize * minSize;
-    option.state.units.forEach(function (glyph) {
-        glyph.size[0] /= minSize;
-        glyph.size[1] /= minSize;
-        glyph.position[0] /= minSize;
-        glyph.position[1] /= minSize;
-    });
-
-    return option;
-}
 
 var tokens = ['jan', 'tu', 'utala', 'mute', 'pona', 'wan', 'lili', 'wan'];
 //var tokens = ['jan', 'utala', 'pona', 'mi', 'wan'];
