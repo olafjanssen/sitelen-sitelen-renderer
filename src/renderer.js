da667c41cf5ddf01d0e0feede1951bb8be00f1f6
Olaf Janssen <o.t.a.janssen@gmail.com>
2016-01-15 02:21:54 +0100

    Cleaned up renderer code even more.

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,6 @@
 var svgNS = "http://www.w3.org/2000/svg",
     xlinkns = "http://www.w3.org/1999/xlink";
 
-
 /**
  * Helper function for creating a DOM element with a full set of attributes and styles.
  *
@@ -33,23 +32,28 @@ function createNewElement(tag, attributes, styles, namespace) {
     return element;
 }
 
-
-function renderOption(option, target, position, sizeParent, sizeParent2) {
+/**
+ * Recursively renders parts of a sentence
+ * @param option    the layout option to render
+ * @param target    the target container
+ * @param position  the position of the current container
+ * @param sizeParent    the size of the parent container
+ * @param sizeParent2   the alternate size of the parent container
+ */
+function renderPartOption(option, target, position, sizeParent, sizeParent2) {
     "use strict";
     var container = target;
 
     if (position) {
-        container = document.createElementNS(svgNS, 'svg');
-        container.style.overflow = 'visible';
         var box = [position[0] * 100 / sizeParent2[0],
             position[1] * 100 / sizeParent2[1],
             sizeParent[0] * 100 / sizeParent2[0],
             sizeParent[1] * 100 / sizeParent2[1]
         ];
 
-        var scale = 1.1;
-        var c = [box[0] + box[2] / 2, box[1] + box[3] / 2];
-        var matrix = [scale, 0, 0, scale, c[0] - scale * c[0], c[1] - scale * c[1]];
+        var scale = 1.1,
+            center = [box[0] + box[2] / 2, box[1] + box[3] / 2],
+            matrix = [scale, 0, 0, scale, center[0] - scale * center[0], center[1] - scale * center[1]];
 
         if (option.separator) {
 
@@ -69,71 +73,78 @@ function renderOption(option, target, position, sizeParent, sizeParent2) {
             target.insertBefore(use, target.firstChild);
         }
 
-        container.setAttribute('preserveAspectRatio', 'none');
-        container.setAttribute('width', '' + box[2]);
-        container.setAttribute('height', '' + box[3]);
-        container.setAttribute('x', '' + box[0]);
-        container.setAttribute('y', '' + box[1]);
-        container.setAttribute('transform', 'matrix(' + matrix.join(',') + ')');
         var scale2 = option.separator ? 1.3 : 1;
-        container.setAttribute('viewBox', [-(100 * scale2 - 100) / 2, -(100 * scale2 - 100) / 2, 100 * scale2, 100 * scale2].join(' '));
-        //container.setAttribute('viewBox',[0, 0, 100, 100].join(' '));
-        target.appendChild(container);
+        var container = createNewElement('svg', {
+            transform: 'matrix(' + matrix.join(',') + ')',
+            viewBox: [-(100 * scale2 - 100) / 2, -(100 * scale2 - 100) / 2, 100 * scale2, 100 * scale2].join(' '),
+            preserveAspectRatio: 'none',
+            height: box[3],
+            width: box[2],
+            x: box[0],
+            y: box[1]
+        }, {
+            overflow: 'visible'
+        }, svgNS);
 
+        target.appendChild(container);
     }
 
     option.state.units.forEach(function (glyph) {
         if (glyph.unit.rule === 'word-glyph') {
-            var use = document.createElementNS(svgNS, 'use');
             var box = [(glyph.position[0] * 100 / option.size[0]),
                 (glyph.position[1] * 100 / option.size[1]),
                 (glyph.size[0] * 100 / option.size[0]),
                 (glyph.size[1] * 100 / option.size[1])
             ];
+            var scale = 1.2,
+                center = [box[0] + box[2] / 2, box[1] + box[3] / 2],
+                matrix = [scale, 0, 0, scale, center[0] - scale * center[0], center[1] - scale * center[1]];
 
-            use.setAttributeNS(xlinkns, 'href', '#tp-wg-' + glyph.unit.token);
-            use.setAttribute('width', '' + box[2]);
-            use.setAttribute('height', '' + box[3]);
-            use.setAttribute('x', '' + box[0]);
-            use.setAttribute('y', '' + box[1]);
+            var use = createNewElement('use', {
+                href: {
+                    ns: xlinkns,
+                    value: '#tp-wg-' + glyph.unit.token
+                },
+                transform: 'matrix(' + matrix.join(',') + ')',
+                viewBox: [0, 0, 100, 100].join(' '),
+                preserveAspectRatio: 'none',
+                height: box[3],
+                width: box[2],
+                x: box[0],
+                y: box[1]
+            }, {}, svgNS);
 
-            var scale = 1.2;
-            var c = [box[0] + box[2] / 2, box[1] + box[3] / 2];
-            var matrix = [scale, 0, 0, scale, c[0] - scale * c[0], c[1] - scale * c[1]];
-
-            use.setAttribute('transform', 'matrix(' + matrix.join(',') + ')');
             container.appendChild(use);
         } else {
-            renderOption(glyph.unit, container, glyph.position, glyph.size, option.size);
+            renderPartOption(glyph.unit, container, glyph.position, glyph.size, option.size);
         }
     });
 
 }
 
-function renderOptions(compoundOptions){
+/**
+ * Render a sentence layout option into a target container.
+ * @param option    the sentence layout option
+ * @param target    the target DOM container
+ */
+function renderSentenceOption(option, target) {
     "use strict";
 
-    for (var i = 0; i < compoundOptions.length; i++) {
-        var option = compoundOptions[i];
-
-        var sentenceContainer = createNewElement('svg',
+    var box = [0, 0,
+            option.size[0] * 100,
+            option.size[1] * 100
+        ],
+        scale = 1.2,
+        sentenceContainer = createNewElement('svg',
             {
                 xmlns: svgNS,
                 'xmlns:xlink': xlinkns,
-                version: 1.1
+                version: 1.1,
+                viewBox: [-(box[2] * scale - box[2]) / 2, -(box[3] * scale - box[3]) / 2, box[2] * scale, box[3] * scale].join(' ')
             }, {
                 display: 'block'
-            }, svgNS);
-
-        var box = [0, 0,
-            option.size[0] * 100,
-            option.size[1] * 100
-        ];
-
-        var scale = 1.2;
-        sentenceContainer.setAttribute('viewBox', [-(box[2] * scale - box[2]) / 2, -(box[3] * scale - box[3]) / 2, box[2] * scale, box[3] * scale].join(' '));
-
-        var innerContainer = createNewElement('svg',
+            }, svgNS),
+        innerContainer = createNewElement('svg',
             {
                 width: box[2],
                 height: box[3],
@@ -143,9 +154,16 @@ function renderOptions(compoundOptions){
                 overflow: 'visible'
             }, svgNS);
 
-        renderOption(option, innerContainer);
+    renderPartOption(option, innerContainer);
 
-        sentenceContainer.appendChild(innerContainer);
-        document.getElementById('sitelen').appendChild(sentenceContainer);
-    }
+    sentenceContainer.appendChild(innerContainer);
+    target.appendChild(sentenceContainer);
+}
+
+function renderOptions(compoundOptions) {
+    "use strict";
+
+    compoundOptions.forEach(function (option) {
+        renderSentenceOption(option, document.getElementById('sitelen'));
+    });
 }
