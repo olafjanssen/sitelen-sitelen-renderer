0fd55664810ecc90afa363ae20138e230353786e
Olaf Janssen <o.t.a.janssen@gmail.com>
2016-03-18 16:56:42 +0100

    Clean up.

--- resources/derivative works/updatesvg.js
+++ resources/derivative works/updatesvg.js
@@ -13,7 +13,7 @@ var glob = require('glob');
 //});
 
 //var files = glob.sync('../../images/glyphs/tp-wg-pre-*.svg');
-var files = glob.sync('../../images/glyphs/tp-wg-li-tall.svg');
+var files = glob.sync('../../images/glyphs/tp-wg-e-tall.svg');
 files.forEach(function (filename) {
     var token = filename.substring(30);
     console.log(token);
