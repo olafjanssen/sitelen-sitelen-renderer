c33abe7637cdf03b1d87b5cc79136c3b94e77f5c
O.T.A. Janssen <o.t.a.janssen@gmail.com>
2025-11-19 17:13:18 +0100

    Improved examples towards wasm use

--- examples/tatoeba/pdfrender.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var page = require('webpage').create();
-page.open('http://localhost:8080/examples/tatoeba/tatoeba-book.html', function(status) {
-    console.log("Status: " + status);
-    if(status === "success") {
-        page.render('example.png');
-    }
-    phantom.exit();
-});
